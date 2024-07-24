fd545b916aad144dfced2a1a22e0629ab0322e2b
manasa y <123yapitsme@gmail.com>
2024-07-24 20:33:18 +0530

    a lot of changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from "../assets/preety_logo.jpeg";
-import '../styles/Navbar.css'
+import "../styles/Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
@@ -8,12 +8,21 @@ function Navbar() {
     <div className="navbar">
       <div className="leftSide">
         <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/services"> Services </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
       </div>
       <div className="rightSide">
-        <Link to='/'> Home </Link>
-        <Link to='/services'> Services </Link>
-        <Link to='/about'> About </Link>
-        <Link to='/contact'> Contact </Link>
+        <Link to="/"> Home </Link>
+        <Link to="/services"> Services </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        <button>
+          <i className="fa-solid fa-bars"></i>
+        </button>
       </div>
     </div>
   );
