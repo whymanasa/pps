5050b3e64d33753e801aff1628a1392b26be1154
manasa y <123yapitsme@gmail.com>
2024-08-23 14:34:39 +0530

    done with the navbar
    started with the homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks? "open" : "close"}>
-        <img src={Logo} />
+        <img src={Logo} alt="text"/>
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/services"> Services </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import BackgroundImage from '../assets/pps_background.png';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>
-      hello world
+    <div className="home">
+      <div className="headerContainer" style={{background:`url(${BackgroundImage})`}}>
+        <h1>Crooked but with Care</h1>
+        <p>Let's Hear 'Em - Your Wish</p>
+        <Link to="/menu">
+          <button>To the salon</button>
+        </Link>
+      </div>
     </div>
   )
 }
